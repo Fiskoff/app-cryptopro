dd98f51bba12582938fe8389c1394a8848d26a99
Sergey Fiskov <fiskovsa@kuzstu.ru>
2026-03-23 11:21:37 +0700

    feat: add exceptions for clint gir vu

--- exceptions.py
+++ exceptions.py
@@ -2,6 +2,17 @@ class CertificateNotFoundError(Exception):
     """Исключение, когда сертификат не найден в хранилище."""
     pass
 
+
 class SigningError(Exception):
     """Исключение ошибки при создании подписи."""
+    pass
+
+
+class GirVuError(Exception):
+    """Базовое исключение для ошибок интеграции с ГИР ВУ."""
+    pass
+
+
+class GirVuAuthError(GirVuError):
+    """Ошибка авторизации (получения токена)."""
     pass
