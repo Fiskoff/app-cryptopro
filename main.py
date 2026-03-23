330ae99ef9a368ff5d54e21410a41046ef466393
Sergey Fiskov <fiskovsa@kuzstu.ru>
2026-03-23 14:29:45 +0700

    feat: add scenario of authorization and receipt of the GIR VU token

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import logging
+
+from app.core import setup_logger, settings, GirVuAuthError, CertificateNotFoundError
+from app.services import CryptoProService
+from app.clients import GetTokenUseCase
+
+
+setup_logger(level="DEBUG")
+logger = logging.getLogger(__name__)
+
 def main():
-    pass
+    logger.info("Запуск процедуры авторизации ГИР ВУ")
+    try:
+        thumbprint = settings.auth.certificate_thumbprint
+        crypto_service = CryptoProService(thumbprint=thumbprint)
+
+        use_case_get_token = GetTokenUseCase(settings=settings.auth, crypto_service=crypto_service)
+        result = use_case_get_token.execute()
+
+        logger.info(f"Token: {result['token']}")
+
+    except CertificateNotFoundError as error:
+        logger.error(f"Ошибка сертификата: {error}")
+
+    except GirVuAuthError as error:
+        logger.error(f"Ошибка авторизации: {error}")
+
+    except Exception as error:
+        logger.exception(f"Критическая ошибка: {error}")
 
 
 if __name__ == "__main__":
