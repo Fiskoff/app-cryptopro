77d0ae00ccedda4543f0511a16d594bccea6491d
Sergey Fiskov <fiskovsa@kuzstu.ru>
2026-03-23 11:37:19 +0700

    fix: import classes from the core module

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,14 +1,18 @@
 from app.core.settings import settings
 from app.core.cryptopro_config import CryptoConstants
 from app.core.exceptions import (
-    SigningError, CertificateNotFoundError,
-    GirVuError, GirVuAuthError
+    SigningError,
+    CertificateNotFoundError,
+    GirVuError,
+    GirVuAuthError
 )
 
 
 __all__ = [
     "settings",
     "CryptoConstants",
-    "SigningError", "CertificateNotFoundError",
-    "GirVuError", "GirVuAuthError"
+    "SigningError",
+    "CertificateNotFoundError",
+    "GirVuError",
+    "GirVuAuthError",
 ]
