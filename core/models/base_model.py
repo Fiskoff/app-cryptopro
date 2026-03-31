25c00f5221cf00f090b39abbe8d811c075037d62
Sergey Fiskov <fiskovsa@kuzstu.ru>
2026-03-31 09:22:20 +0700

    docs: add documentation

--- core/models/base_model.py
+++ core/models/base_model.py
@@ -2,4 +2,5 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 class BaseModel(DeclarativeBase):
+    """Базовая модель всех ORM моделей. Содержит метаданные всех моделей, которые от неё наследованные"""
     pass
